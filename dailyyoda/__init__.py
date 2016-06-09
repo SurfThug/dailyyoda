17830d71e4fced0decef320e0f59a0fa51444aec
Sean Crosby <seanmcrosby@gmail.com>
2016-06-09 23:09:49 +0000

    first working version

--- dailyyoda/__init__.py
+++ dailyyoda/__init__.py
@@ -2,34 +2,55 @@ import os
 from datetime import date, timedelta
 from dateutil.parser import parse
 
-# convert the string into a date object
-def parsedate(datestr):
-  d = None
+class dailyyoda:
 
-  if datestr == "today":
-    d = date.today()
-  elif datestr == "yesterday":
-    d = date.today() - timedelta(days=1)
-  elif datestr == "tomorrow":
-    d = date.today() + timedelta(days=1)
-  else:
-    # try parsing it
-    d = parse(datestr)
-  return d
+  def __init__(self, dbname):
+    print "Yoda Quote of the Day"
+    filename = None
+    if dbname == 'test':
+      filename = 'test.db'
+    elif dbname == 'yoda':
+      filename = 'yoda.db'
+    else:
+      raise ValueError("Unknown dbname: " + dbname + ".  Acceptable options: test, yoda")
 
-# return the day of year from a date object
-def date2num(d):
-  tt = d.timetuple()
-  return tt.tm_yday
+    self.db = self.load_db(filename)
 
-def read_db():
-  this_dir, this_filename = os.path.split(__file__)
-  dbpath = os.path.join(this_dir, "quotes.db")
-  quotes = [line.rstrip('\n') for line in open(dbpath)]
+  # convert the string into a date object
+  def parse_date(self, datestr):
+    d = None
 
-# return the quote from a given index
-def getquote(num):
-  quotes = read_db()
+    if datestr == "today":
+      d = date.today()
+    elif datestr == "yesterday":
+      d = date.today() - timedelta(days=1)
+    elif datestr == "tomorrow":
+      d = date.today() + timedelta(days=1)
+    else:
+      # try parsing it
+      d = parse(datestr)
+    return d
 
-  index = num % len(quotes)
-  return quotes[index] 
+  # return the day of year from a date object
+  def date2num(self, d):
+    tt = d.timetuple()
+    return tt.tm_yday
+
+  # load the quote db from file
+  def load_db(self, filename):
+    this_dir, this_filename = os.path.split(__file__)
+    dbpath = os.path.join(this_dir, filename)
+    db = [line.rstrip('\n') for line in open(dbpath)]
+    return db
+
+  # return the quote from a given index
+  def lookup_quote(self, num):
+    index = num % len(self.db)
+    return self.db[index] 
+
+  # return the quote from a given date
+  def get_quote(self, datestr):
+    d = self.parse_date(datestr)
+    num = self.date2num(d)
+    quote = self.lookup_quote(num)
+    return quote
